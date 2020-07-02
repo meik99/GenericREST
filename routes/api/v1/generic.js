16bea839514cf3978e5ef29740b36c67a37d27e5
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2020-07-02 21:40:44 +0200

    added delete and put

--- routes/api/v1/generic.js
+++ routes/api/v1/generic.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongodb = require('mongodb');
 const router = express.Router();
 const getMongoCollection = require('../../../db/mongo');
 
@@ -51,5 +52,51 @@ router.post('/generic/many', (req, res) => {
         });
     }
 });
+
+router.put('/generic', (req, res) => {
+    if (!req.query.collection) {
+        res.send({code: 400, message: 'Collection name not given'})
+    } else {
+        getMongoCollection(req.query.collection, (collection) => {
+            const query = { _id: mongodb.ObjectId(req.body._id) };
+            delete req.body._id;
+            collection.findOneAndUpdate(query, {$set: req.body } , { upsert: true}, (err, result) => {
+                if (err) {
+                    res.send({code: 500, message: err});
+                } else {
+                    getMongoCollection(req.query.collection, (collection) => {
+                        collection.find(query).toArray((err, result) => {
+                            if (err) {
+                                res.send({code: 500, message: err});
+                            } else {
+                                res.send(result);
+                            }
+                        });
+                    });
+                }
+            });
+        });
+    }
+});
+
+router.delete('/generic', (req, res) => {
+    if (!req.query.collection) {
+        res.send({code: 400, message: 'Collection name not given'})
+    } else if (!req.query.query) {
+        res.send({code: 400, message: 'Query not given'})
+    } else {
+        getMongoCollection(req.query.collection, (collection) => {
+            const query = JSON.parse(req.query.query);
+            query._id = mongodb.ObjectId(query._id);
+            collection.deleteOne(query, (err, result) => {
+                if (err) {
+                    res.send({code: 500, message: err});
+                } else {
+                    res.send(result.result);
+                }
+            });
+        });
+    }
+})
 
 module.exports = router;
